eb334336da57484dc31765dc8af2c31358cbcbef
Darlyn <darlynreynoso74@gmail.com>
2022-10-10 17:36:59 +0200

    autosCertifica2 version 1.0.6 promo + vehiculos

--- src/components/Promo.jsx
+++ src/components/Promo.jsx
@@ -4,8 +4,8 @@ const Promo = () => {
   return (
     <div className="container-fluid fondoPromo ">
       <h2 className="parpadeoPromo h2Promo">MONTATE CON RD$150,000</h2>
-      <p className="textoPromo parpadeoPromo">APROBACIÓN INMEDIATA!!</p>
-      <p className="textoPromo2 parpadeoPromo">NO IMPORTA CRÉDITO.</p>
+      <p className="textoPromo parpadeoPromo">¡APROBACIÓN INMEDIATA!</p>
+      <p className="textoPromo2 parpadeoPromo">¡NO IMPORTA CRÉDITO!</p>
     </div>
   );
 };
